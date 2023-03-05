60ac79819f135479fb8524e457355926e8641774
vijayaprm <vijayapratapreddy.m@gmail.com>
2023-03-05 15:37:08 +0530

    started with auth

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './Auth.css' 
+import icon from '../../assets/icon.png'
 
 const Auth = () => {
+  const [isSignup   ,setIsSignup] = useState(false)
+
+  const handleSwitch =() => {
+     setIsSignup(!isSignup)
+  }
   return (
-    <div>
-   
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat praesentium inventore, tempora commodi voluptatem dolor harum eos libero voluptatum recusandae iusto provident aspernatur! Magni architecto repellendus distinctio sint, animi labore.</h1>
-    </div>
+    <section className='auth-section'>
+      <div className='auth-container'>
+            {!isSignup && <img src={icon} alt='doubt' className='login-logo'></img>}
+            <form >
+              {
+                isSignup && (
+                    <label htmlFor="email">
+                      <h4>Email</h4>
+                       <input type="email" name='email' id='email' />
+                    </label>
+                    
+                )
+              }
+              <label htmlFor="email">
+                <h4>Email</h4>
+                <input type="email" name='email' id='email' />
+              </label>
+              <label htmlFor="password">
+                <div>
+                <h4>Password</h4>
+                { !isSignup && <h4>Forgot Password</h4>}
+                </div>
+                <input type="password" name='password' id='password' />
+              </label>
+              <button type='submit' className='auth-btn'>{isSignup ? 'Sign up' : 'Log In'}</button>
+            </form>
+            <p>
+              {isSignup ? 'Already Have an Acoount?' : "Don't Have an Account?"}
+              <button type='button' className='handle-switch-btn' onClick={handleSwitch}>{isSignup ? "Log in" : 'Sign up'}</button>
+            </p>
+      </div>
+    </section>
   )
 }
 
