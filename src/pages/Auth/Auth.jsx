c9d45621af82eacf7551d092a40bf1759563cbca
vijayaprm <vijayapratapreddy.m@gmail.com>
2023-03-10 15:43:19 +0530

    auth complete started with home

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import './Auth.css' 
 import icon from '../../assets/icon.png'
+import AboutAuth from './AboutAuth'
 
 const Auth = () => {
   const [isSignup   ,setIsSignup] = useState(false)
@@ -10,14 +11,15 @@ const Auth = () => {
   }
   return (
     <section className='auth-section'>
-      <div className='auth-container'>
+      { isSignup && <AboutAuth />}
+      <div className='auth-container-2'>
             {!isSignup && <img src={icon} alt='doubt' className='login-logo'></img>}
             <form >
               {
                 isSignup && (
-                    <label htmlFor="email">
-                      <h4>Email</h4>
-                       <input type="email" name='email' id='email' />
+                    <label htmlFor="name">
+                      <h4>Display Name</h4>
+                       <input type="text" name='name' id='name' />
                     </label>
                     
                 )
@@ -29,11 +31,33 @@ const Auth = () => {
               <label htmlFor="password">
                 <div>
                 <h4>Password</h4>
-                { !isSignup && <h4>Forgot Password</h4>}
-                </div>
                 <input type="password" name='password' id='password' />
+                { !isSignup && <p style={{ color: "#007ac6", fontSize: "13px" }}>
+                  forgot password?
+                </p>}
+                </div>
+                
+                { isSignup && <p style={{ color:"#666767",fontSize:"13px"}}>Passwords must contian atleast 8 characters<br/> including 1 letter,1 number and 1 symbol</p>                }
               </label>
+              {
+                isSignup && (
+                  <label htmlFor='check'>
+                      <input type="checkbox" id='check'/>
+                      <p style={{fontSize:"13px"}}>Agreeing to all conditions </p>
+                  </label>
+                )
+              }
               <button type='submit' className='auth-btn'>{isSignup ? 'Sign up' : 'Log In'}</button>
+               {
+                isSignup && (
+                  <p style={{ color:"#666767",fontSize:"13px"}}>
+                    By Clicking "Sign up",you agree to our 
+                    <span style={{ color:"#007ac6"}}>Terms and Conditions</span>,<br/>
+                    <span style={{ color:"#007ac6"}}>Privacy policy</span> and 
+                    <span style={{ color:"#007ac6"}}>Cookie policy</span>
+                  </p>
+                )
+               }
             </form>
             <p>
               {isSignup ? 'Already Have an Acoount?' : "Don't Have an Account?"}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import HomeMainbar from '../../components/HomeMainbar/HomeMainbar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container-1'>
+      <LeftSidebar />
+      <div className='home-container-2'>
+            <HomeMainbar />
+            <RightSidebar />
+      </div>
+    </div>
   )
 }
 
